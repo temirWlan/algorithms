812473888e5d92e001a0d3c2e4d70b5093ab3b31
Temirlan <temirlan.balguzhin@bk.ru>
2020-11-24 15:00:25 +0600

    feat: install packages for testing and start write tests

--- src/bubble-sort.js
+++ src/bubble-sort.js
@@ -1,11 +1,13 @@
 const arr = Array(100).fill(0).map(() => Math.floor(Math.random() * 1000) - 500)
 
-for (let i = 0; i < arr.length - 1; i++) {
-	for (let j = 0; j < arr.length - 1 - i; j++) {
-		if (arr[j] > arr[j + 1]) {
-			let temp = arr[j];
-			arr[j] = arr[j + 1];
-			arr[j + 1] = temp;
+module.exports = function bubbleSort(arr) {
+	for (let i = 0; i < arr.length - 1; i++) {
+		for (let j = 0; j < arr.length - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				let temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
 		}
 	}
 }
---
+++ test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const { expect, assert } = chai;
+
+const bubbleSort = require('./src/bubble-sort.js');
+
+describe('Sorting algorithm', () => {
+	expect(bubbleSort).to.be
+})
