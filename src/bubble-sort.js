634f50e1b448f2f1de0eb32912eb7ea7d9110685
Temirlan <temirlan.balguzhin@bk.ru>
2020-11-24 13:51:07 +0600

    feat: add bubble sort

---
+++ src/bubble-sort.js
@@ -0,0 +1,11 @@
+const arr = Array(100).fill(0).map(() => Math.floor(Math.random() * 1000) - 500)
+
+for (let i = 0; i < arr.length - 1; i++) {
+	for (let j = 0; j < arr.length - 1 - i; j++) {
+		if (arr[j] > arr[j + 1]) {
+			let temp = arr[j];
+			arr[j] = arr[j + 1];
+			arr[j + 1] = temp;
+		}
+	}
+}
